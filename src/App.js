d5029f472de08161033d198027bc01ab01384af2
Oswald <stashkozzf@gmail.com>
2023-05-10 19:08:21 +0300

    added textarea

--- src/App.js
+++ src/App.js
@@ -1,14 +1,20 @@
+import { useState } from 'react'
+
 import Input from './Input'
 import Stats from './Stats'
 import List from './List'
-
 import './styles/app.css'
+
 function App() { 
+    const [stats, setStats] = useState({
+        wordCount: 0,
+    });
+
     return(
         <div className='container'>
             <Stats />
             <main className='main'>
-                <Input />
+                <Input stats={stats} setStats={setStats} />
                 <List />    
             </main>
         </div>
--- src/Input.js
+++ src/Input.js
@@ -1,8 +1,18 @@
 import './styles/input.css'
 
-function Input(){
-    return
-        <textarea className='input'/>
+function Input({stats, setStats}){
+    const handleChange = (e) =>{
+        setStats({...stats ,
+                wordCount: e.target.value.split(/\s+/).length
+            });
+    }
+    return(
+        <textarea
+            onChange={handleChange}
+            className='input'
+            placeholder='Paste your text here'
+        />
+    )
 }
 
 export default Input;
